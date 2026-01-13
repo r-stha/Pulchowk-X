8918abbe22281966353b3ee91d33e2dcb00b9968
Rohit Yadav <rohityadav.se@gmail.com>
2026-01-13 21:45:15 +0545

    refactor: Clean up code formatting and improve readability in student registration service

--- backend/src/services/studentRegister.service.ts
+++ backend/src/services/studentRegister.service.ts
@@ -3,26 +3,24 @@ import { StudentInput } from "../types/studentRegister.js";
 import { db } from "../lib/db.js";
 import { eq } from "drizzle-orm";
 
-export async function createStudentProfile(data: StudentInput){
-    
-try{ 
+export async function createStudentProfile(data: StudentInput) {
+    try {
+        const existingUser = await db.query.students.findFirst({
+            where: eq(students.authStudentId, data.authStudentId)
+        })
+        if (existingUser) {
+            console.log("existing user")
+            return;
+        }
 
- const existingUser = await db.query.students.findFirst({
-    where: eq(students.authStudentId, data.authStudentId )
- })
-if(existingUser){
-    console.log("existing user")
-    return;
+        await db.insert(students).values(data);
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
-    await db.insert(students).values(data);
-}catch(error){
-    console.log(error.message);
-}
-}
 
-
-export async function getStudentProfile(authStudentId: string){
-    const [student] = await db .select().from(students).where(eq(students.authStudentId, authStudentId));
+export async function getStudentProfile(authStudentId: string) {
+    const [student] = await db.select().from(students).where(eq(students.authStudentId, authStudentId));
     return student;
 }
